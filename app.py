9f9074b72cd834ca821a8289c0473a51bb1cff72
ekonotim <tim@ekonopac.com>
2025-05-23 14:34:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def generate_pdf():
     return send_file(buffer, as_attachment=True, download_name=filename, mimetype='application/pdf')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
